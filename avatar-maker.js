d7478731c2f6fed0e9ed1424a4a60b15aee8f49f
George <92999775+gloyens@users.noreply.github.com>
2022-08-14 16:41:11 +0100

    Create next/previous buttons

--- avatar-maker.js
+++ avatar-maker.js
@@ -1,20 +1,39 @@
+let cwd = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
+
+// Reference number for each image, e.g. base1.png
+let baseNum = 1;
+
 window.onload = () => { // This is an arrow function, rewritten from "window.onload = function() {"
   var base = document.createElement("img");
-  var baseNum = Math.floor(Math.random() * 6) + 1; // Multiply Math.random() by number of files
-  var baseName = "/img/base" + baseNum + ".png";
-  var cwd = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
+  //var baseNum = Math.floor(Math.random() * 6) + 1; // Multiply Math.random() by number of files
     
-  base.src = cwd + baseName;
-  console.log(base.src);
-
-  base.onload = () => { buildAvatar(); }
-
-  function buildAvatar() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    canvas.height = canvas.width;
-    console.log(ctx);
-
-    ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
-  }
+  base.src = cwd + "/img/base" + baseNum + ".png";
+  base.onload = () => { buildAvatar(base); }
 }
+
+function buildAvatar(layer) {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.height = canvas.width;
+
+  ctx.drawImage(layer, 0, 0, canvas.width, canvas.height);
+}
+
+function baseChange(direction) {
+  var base = document.createElement("img");
+
+  switch(direction) {
+    case "previous":
+      if (baseNum == 1) { baseNum = 6; }
+      else { baseNum -= 1; };
+      break;
+    case "next":
+      if (baseNum == 6) { baseNum = 1; }
+      else { baseNum += 1; }
+      break;
+  }
+
+  base.src = cwd + "/img/base" + baseNum + ".png";
+  base.onload = () => { buildAvatar(base); }
+}
+

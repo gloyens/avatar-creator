22c84dd3e0d2cc2dacb67f42de001d74613ff6c3
George <92999775+gloyens@users.noreply.github.com>
2022-08-17 15:16:53 +0100

    Add hair and eyes options

--- avatar-maker.js
+++ avatar-maker.js
@@ -4,14 +4,22 @@ let cwd = window.location.pathname.substring(0, window.location.pathname.lastInd
 let baseNum = 1;
 let hairNum = 1;
 let hairColourNum = 1;
+let eyesNum = 1;
 
 // Create images
 let base = document.createElement("img");
 let hair = document.createElement("img");
+let eyes = document.createElement("img");
 
 window.onload = () => { // This is an arrow function, rewritten from "window.onload = function() {"
   base.src = cwd + "/img/base" + baseNum + ".png";
   base.onload = () => { buildAvatar(); }
+
+  hair.src = cwd + "/img/hair" + hairNum + "-" + hairColourNum + ".png";
+  hair.onload = () => { buildAvatar(); }
+
+  eyes.src = cwd + "/img/eyes" + eyesNum + ".png";
+  eyes.onload = () => { buildAvatar(); }
 }
 
 function buildAvatar() {
@@ -20,7 +28,8 @@ function buildAvatar() {
   canvas.height = canvas.width;
 
   ctx.drawImage(base, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(hair, 1, 0, canvas.width, canvas.height);
+  ctx.drawImage(hair, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(eyes, 0, 0, canvas.width, canvas.height);
 }
 
 function baseChange(direction) {
@@ -39,10 +48,26 @@ function baseChange(direction) {
   base.onload = () => { buildAvatar(); }
 }
 
+function hairChange(direction) {
+  switch(direction) {
+    case "previous":
+      if (hairNum == 1) { hairNum = 2; }
+      else { hairNum -= 1; };
+      break;
+    case "next":
+      if (hairNum == 2) { hairNum = 1; }
+      else { hairNum += 1; }
+      break;
+  }
+
+  hair.src = cwd + "/img/hair" + hairNum + "-" + hairColourNum + ".png";
+  hair.onload = () => { buildAvatar(); }
+}
+
 function hairColourChange(direction) {
   switch(direction) {
     case "previous":
-      if (hairColourNum == 1) { trimhairColourNumNum = 5; }
+      if (hairColourNum == 1) { hairColourNumNum = 5; }
       else { hairColourNum -= 1; };
       break;
     case "next":
@@ -53,4 +78,20 @@ function hairColourChange(direction) {
 
   hair.src = cwd + "/img/hair" + hairNum + "-" + hairColourNum + ".png";
   hair.onload = () => { buildAvatar(); }
+}
+
+function eyesChange(direction) {
+  switch(direction) {
+    case "previous":
+      if (eyesNum == 1) { eyesNum = 3; }
+      else { eyesNum -= 1; };
+      break;
+    case "next":
+      if (eyesNum == 3) { eyesNum = 1; }
+      else { eyesNum += 1; }
+      break;
+  }
+
+  eyes.src = cwd + "/img/eyes" + eyesNum + ".png";
+  eyes.onload = () => { buildAvatar(); }
 }
